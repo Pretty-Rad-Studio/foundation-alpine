39d05984c954a24513e2d3eb1f05bb6f91b3fc11
Nicole Dominguez <sodevious.net@gmail.com>
2022-12-05 21:19:41 -0500

    Move over styles and fonts

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -8,6 +8,73 @@ const path = require('path')
 
 module.exports = {
   theme: {
+    container: {
+      padding: {
+        default: '1rem',
+      },
+      screens: {
+        'sm': '100%',
+        'md': '100%',
+        'lg': '1440px',
+      },
+      fontWeight: {
+        light: 300,
+        normal: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        extrabold: 800,
+        black: 900,
+      },
+      colors: {
+        white: '#F1F0EF',
+        black: '#0C140B',
+        gray: '#858985',
+        transparent: '#00000000'
+      },
+      fontSize: {
+        'heading': ['48px', '1'],
+        'headingMobile': ['32px', '1'],
+        'bodyLarge': ['16px', '1.5'],
+        'bodySmall': ['12px', '1.6'],
+        'label': ['10px', '1.2'],
+      },
+      lineHeight: {
+        '48': '3rem',
+        '32': '2rem',
+        '24': '1.5rem',
+        '16': '1rem',
+        '12': '0.75rem'
+      },
+      boxShadow: {
+        default: "box-shadow: 0px 8px 48px rgba(0, 0, 0, 0.08);",
+      },
+      blur: {
+        default: '32px',
+      },
+      extend: {
+        transitionProperty: {
+          'backgroundColor': 'background',
+          'opacity': 'opacity',
+        },
+        transitionDuration: {
+          '0': '0ms',
+          '300': '300ms',
+        },
+        textOpacity: {
+          '85': '0.85',
+        },
+        backgroundImage: theme => ({
+          'hero-pattern': "url('../images/Hero.png')",
+        }),
+        inset: {
+          '2': '2rem',
+        }
+      },
+    },
+    fontFamily: {
+      default: ["NeueHaasGroteskDisp"],
+    },
     extend: {},
   },
   plugins: [],
